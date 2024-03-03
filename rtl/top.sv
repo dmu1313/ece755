2b771647234fe72f82834bc0c9832814a6b4ac04
Daniel Mu <dsmu@colossus.cae.wisc.edu>
2024-03-02 23:06:08 -0600

    Finished pipelined implementation

--- rtl/top.sv
+++ rtl/top.sv
@@ -26,32 +26,22 @@ module top #(
   logic signed [hidden_layer_output_size-1:0] y4_relu, y5_relu, y6_relu, y7_relu;
   logic signed [output_size-1:0] y8, y9;
 
-//  logic out0_ff, out1_ff;
-  logic ready_input, ready_hidden_layer, ready_relu_layer, ready_output_layer;
+  logic ready_hidden_layer, ready_relu_layer, ready_output_layer;
 
-  assign out0_ready = ready_relu_layer;
-  assign out1_ready = ready_relu_layer;
+  assign y0 = x0;
+  assign y1 = x1;
+  assign y2 = x2;
+  assign y3 = x3;
+
+  assign out0_ready = ready_output_layer;
+  assign out1_ready = ready_output_layer;
 
   always_ff @(posedge clk) begin
-    ready_input <= in_ready;
-    ready_hidden_layer <= ready_input;
+    ready_hidden_layer <= in_ready;
     ready_relu_layer <= ready_hidden_layer;
     ready_output_layer <= ready_relu_layer;
 
     if (in_ready) begin
-      y0 <= x0;
-      y1 <= x1;
-      y2 <= x2;
-      y3 <= x3;
-    end
-    else begin
-      y0 <= y0;
-      y1 <= y1;
-      y2 <= y2;
-      y3 <= y3;
-    end
-
-    if (ready_input) begin
       y4 <= (y0 * w04) + (y1 * w14) + (y2 * w24) + (y3 * w34);
       y5 <= (y0 * w05) + (y1 * w15) + (y2 * w25) + (y3 * w35);
       y6 <= (y0 * w06) + (y1 * w16) + (y2 * w26) + (y3 * w36);
@@ -86,16 +76,6 @@ module top #(
       y9 <= y9;
     end
   end
-
-//  always_comb begin
-//    y4 = (y0 * w04) + (y1 * w14) + (y2 * w24) + (y3 * w34);
-//    y5 = (y0 * w05) + (y1 * w15) + (y2 * w25) + (y3 * w35);
-//    y6 = (y0 * w06) + (y1 * w16) + (y2 * w26) + (y3 * w36);
-//    y7 = (y0 * w07) + (y1 * w17) + (y2 * w27) + (y3 * w37);
-//
-//    y8 = (y4_relu * w48) + (y5_relu * w58) + (y6_relu * w68) + (y7_relu * w78);
-//    y9 = (y4_relu * w49) + (y5_relu * w59) + (y6_relu * w69) + (y7_relu * w79);
-//  end 
 
   assign out0 = y8[output_size-2:0];
   assign out1 = y9[output_size-2:0];
